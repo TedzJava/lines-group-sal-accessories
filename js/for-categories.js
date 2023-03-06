0f73de1f57f7edc4479f8e8be397b450c14556c2
Teddy El Khoury <teddy.khoury6@gmail.com>
2023-03-06 18:17:58 +0200

    main

---
+++ js/for-categories.js
@@ -0,0 +1,278 @@
+class Items{
+    constructor(type, name, code, src){
+        this.type = type;
+        this.name = name;
+        this.code = code;
+        this.src = src;
+    }
+}
+
+const bracelet = [
+    new Items ('bracelet', 'Silver Bracelet', 'Code: PTY0673', 'bracelet 1.jpg')
+];
+const comb = [
+    new Items ('comb', 'Hair Comb 1064', 'Hair Comb 1064', 'comb 1.jpg'),
+    new Items ('comb', 'Comb Pearl', 'HS-J4744', 'comb 2.jpg'),
+    new Items ('comb', 'Rhinestone, Alloy flower/ leaf, Pearl', 'HP268', 'comb 4.jpg'),
+    new Items ('comb', 'Pearl Bridal Combs', 'FS044', 'comb 5.jpg'),
+    new Items ('comb', 'Rhinestone', 'HP461', 'comb 6.jpg'),
+    new Items ('comb', 'Ceramics pearls combs', 'Code: S8440', 'comb 7.jpg'),
+    new Items ('comb', 'Crystal Pearl Ornaments', 'Crystal Pearl Ornaments', 'comb 9.jpg'),
+    new Items ('comb', 'Hair Comb 1069', 'Hair Comb 1069', 'comb 10.jpg'),
+    new Items ('comb', 'Pearl, Crystal, Rhinestone', 'HP406', 'comb 11.jpg'),
+    new Items ('comb', 'Pearl, Crystal, Rhinestone 2', 'HP406-2', 'comb 12.jpg'),
+    new Items ('comb', 'Tiara Bohemdressorehead', 'HP466', 'comb 14.jpg'),
+    new Items ('comb', 'HAIR PINS 1065', 'HAIR PINS 1065', 'comb 15.jpg'),
+    new Items ('comb', 'Vintage Big Crystal', 'HS-J4478', 'comb 16.jpg'),
+    new Items ('comb', 'Combs Clips', 'HS-J4999', 'comb 19.jpg'),
+    new Items ('comb', 'ELEVEN JIAN', 'ELEVEN JIAN', 'comb 20.jpg'),
+    new Items ('comb', 'Rhinestone-Pearl', 'HP266', 'comb 24.jpg'),
+    new Items ('comb', 'Pearls Cubic Zircon', 'SL-ZC2017031324', 'comb 25.jpg'),
+    new Items ('comb', 'HAIR COMB 1066', 'SL-HS1064', 'comb 29.jpg'),
+    new Items ('comb', 'Leaf Flower Pearl', 'HM6075', 'comb 31.jpg'),
+    new Items ('comb', 'Hair comb 32', 'Hair comb 32', 'comb 32.jpg'),
+    new Items ('comb', 'Hair comb 33', 'Hair comb 33', 'comb 33.jpg'),
+    new Items ('comb', 'HAIR COMB 1067', 'HAIR COMB 1067', 'comb 34.jpg'),
+    new Items ('comb', 'Silver Pearl Bead Crystal', 'S8210', 'comb 35.jpg')
+];
+const crown = [
+    new Items ('crown', 'Cubic Zircon Tiara', 'GH0025', 'crown 1.jpg'),
+    new Items ('crown', 'Flower Tiara', 'G4618', 'crown 2.jpg'),
+    new Items ('crown', 'Platinum Plated, 24K Gold Plated', 'Platinum Plated, 24K Gold Plated', 'crown 3.jpg'),
+    new Items ('crown', 'Cubic Zircon Crown', 'SL-GSHG0122', 'crown 4.jpg'),
+    new Items ('crown', 'Pearl Headpiece', 'BC6092', 'crown 5.jpg'),
+    new Items ('crown', 'Platinum Plated, 24K Gold Plated - 2', 'Platinum Plated, 24K Gold Plated - 2', 'crown 6.jpg'),
+    new Items ('crown', 'Vintage Crystal Tiara', 'HG239', 'crown 10.jpg'),
+    new Items ('crown', 'Ceramic alloy crystal', 'O873', 'crown 11.jpg'),
+    new Items ('crown', 'Crystal crown', 'Code: HG191', 'crown 12.jpg'),
+    new Items ('crown', 'Platinum Plated', 'Platinum Plated', 'crown 13.jpg'),
+    new Items ('crown', 'Vintage Retro Gold', 'Hana0036420', 'crown 14.jpg'),
+    new Items ('crown', 'Platinum Plated Zircon', '116# MODEL', 'crown 16.jpg'),
+    new Items ('crown', 'Silver band', 'PTY067', 'crown 20.jpg'),
+    new Items ('crown', 'Elegant Crystal Tiara', 'HG105', 'crown 21.jpg'),
+    new Items ('crown', 'Copper zircon platinum', 'Copper zircon platinum', 'crown 23.jpg'),
+    new Items ('crown', 'Platinum Plated Zircon', 'Platinum Plated Zircon', 'crown 24.jpg'),
+    new Items ('crown', 'Zircon crystal Tiara', 'Zuan1307', 'crown 25.jpg'),
+    new Items ('crown', 'Retro Dark Baroque', 'W-HG1078', 'crown 26.jpg'),
+    new Items ('crown', '24K Gold Plated', 'GSHG-183#', 'crown 27.jpg'),
+    new Items ('crown', '24K Gold Plated - 2', '24K Gold Plated - 2', 'crown 28.jpg'),
+    new Items ('crown', 'Platinum Plated Crown', 'Platinum Plated Crown', 'crown 29.jpg'),
+    new Items ('crown', 'Cubic Zircon', 'GS0337', 'crown 30.jpg'),
+    new Items ('crown', 'Butterfly flower crystal golden', 'Butterfly flower crystal golden', 'crown 31.jpg'),
+    new Items ('crown', 'Ivory Pearl Flowers Headband', 'BC6109', 'crown 32.jpg'),
+    new Items ('crown', 'Alloy Crystal Rhinestone', 'H1184', 'crown 34.jpg'),
+    new Items ('crown', 'Platinum Plated Crown- - 2', 'Platinum Plated Crown - 2', 'crown 33.jpg'),
+    new Items ('crown', 'Platinum Plated, Gold Plated ZIRCON', 'Platinum Plated, Gold Plated ZIRCON', 'crown 35.jpg'),
+    new Items ('crown', 'Platinum Plated, 24K Gold Plated', 'Platinum Plated, 24K Gold Plated', 'crown 36.jpg'),
+    new Items ('crown', 'Platinum Plated - 2', 'Platinum Plated - 2', 'crown 37.jpg'),
+    new Items ('crown', 'Platinum Plated - 3', 'Platinum Plated - 3', 'crown 38.jpg')
+];
+const flower = [
+    new Items ('flower', 'White', 'PTY0667', 'flower 1.jpg'),
+    new Items ('flower', 'Silver plated', 'WBT024', 'flower 2.jpg'),
+    new Items ('flower', 'Pearl', 'HF24', 'flower 3.jpg'),
+    new Items ('flower', 'Crystal rhinestone', 'FBY03295', 'flower 4.jpg'),
+    new Items ('flower', 'White', 'WBT043', 'flower 6.jpg'),
+    new Items ('flower', 'White', 'WBT060', 'flower 7.jpg'),
+    new Items ('flower', 'Crystal diamond', 'T4177', 'flower 10.jpg'),
+    new Items ('flower', 'Silver Pearl crystal', 'PTY0669', 'flower 11.jpg'),
+    new Items ('flower', 'pearl crystal feather', 'HF03', 'flower 12.jpg'),
+    new Items ('flower', 'Crystal feather', 'PTY0668-1', 'flower 13.jpg'),
+    new Items ('flower', 'Silver feather', 'PTY0266-3', 'flower 14.jpg'),
+    new Items ('flower', 'Silver crystal', 'PTY0261', 'flower 15.jpg'),
+    new Items ('flower', 'Natural', 'WBT051', 'flower 16.jpg'),
+    new Items ('flower', 'Pearl Silver', 'WBT041', 'flower 17.jpg'),
+    new Items ('flower', 'Silver plated', 'PTY0671', 'flower 18.jpg'),
+    new Items ('flower', 'Gold plated', 'PTY0672', 'flower 21.jpg'),
+    new Items ('flower', 'Crystal diamond', 'PTY0668-2', 'flower 22.jpg'),
+    new Items ('flower', 'Flower handpieces', 'Tu007', 'flower 32.jpeg'),
+    new Items ('flower', 'White crystal', 'WBT034', 'flower 24.jpg'),
+    new Items ('flower', 'White red silk diamond', 'FBY0394', 'flower 25.jpeg'),
+    new Items ('flower', 'Flower handpieces', 'Tu001', 'flower 26.jpeg'),
+    new Items ('flower', 'Flower handpieces', 'Tu002', 'flower 27.jpeg'),
+    new Items ('flower', 'Flower handpieces', 'Tu003', 'flower 28.jpeg'),
+    new Items ('flower', 'Flower handpieces ', 'Tu004', 'flower 29.jpeg'),
+    new Items ('flower', 'Flower handpieces', 'Tu005', 'flower 30.jpeg'),
+    new Items ('flower', 'Flower handpieces', 'Tu006', 'flower 31.jpeg'),
+    new Items ('flower', 'Silver Bouquets', 'PTY0266-5', 'flower 23.jpg')
+];
+const glasses = [
+    new Items ('glasses', 'Handmade Toasting Glass - 1', 'Handmade Toasting Glass - 1', 'glasses 2.jpg'),  
+    new Items ('glasses', 'Handmade Toasting Glass - 2', 'Handmade Toasting Glass - 2', 'glasses 3.jpg'),  
+    new Items ('glasses', 'Handmade Toasting Glass - 3', 'Handmade Toasting Glass - 3', 'glasses 4.jpg'),  
+    new Items ('glasses', 'Handmade Toasting Glass - 4', 'Handmade Toasting Glass - 4', 'glasses 5.jpg'),  
+    new Items ('glasses', 'Handmade Toasting Glass - 5', 'Handmade Toasting Glass - 5', 'glasses 6.jpg'),  
+    new Items ('glasses', 'Handmade Toasting Glass - 6', 'Handmade Toasting Glass - 6', 'glasses 7.jpg'),  
+    new Items ('glasses', 'Handmade Toasting Glass - 7', 'Handmade Toasting Glass - 7', 'glasses 8.jpg'),  
+    new Items ('glasses', 'Handmade Toasting Glass - 8', 'Handmade Toasting Glass - 8', 'glasses 9.jpg'),  
+    new Items ('glasses', 'Handmade Toasting Glass', 'PTY0723', 'glasses 10.jpg')
+];
+const headband = [
+    new Items ('headband', 'Rhinestone-Bead-Pearl', 'HP288', 'headband 1.jpg'),
+    new Items ('headband', 'Tiara Bohemdressorehead', 'HP466 - 2', 'headband 2.jpg'),
+    new Items ('headband', 'Rhinestone', 'HP453', 'headband 3.jpg'),
+    new Items ('headband', 'Rhinestone Tiara Handmade', 'HP433', 'headband 4.jpg'),
+    new Items ('headband', 'Cubic Zirconia gold silver plated', '228', 'headband 5.jpg'),
+    new Items ('headband', 'Rhinestone-Pearl', 'HP266 - 2', 'headband 6.jpg'),
+    new Items ('headband', 'Platinum Plated', '83#', 'headband 7.jpg'),
+    new Items ('headband', 'Flower-Pearl-crystal', 'HP467', 'headband 8.jpg'),
+    new Items ('headband', 'Rhinestone-Crystal-Pearl', 'HP453 - 2', 'headband 9.jpg'),
+    new Items ('headband', 'Rhinestone/Alloy flower', 'HP384', 'headband 10.jpg'),
+    new Items ('headband', 'Baroque Flower Rhinestone Pearl', 'BC6121', 'headband 11.jpg'),
+    new Items ('headband', 'Pearls Beads Baroque', 'HB6596', 'headband 12.jpg'),
+    new Items ('headband', 'Rhinestone crystal leaf', 'SG0805', 'headband 13.jpg'),
+    new Items ('headband', 'Rhinestone Headbands', 'HP351', 'headband 14.jpg'),
+    new Items ('headband', 'Pearl Crystal', 'HP357', 'headband 15.jpg'),
+    new Items ('headband', 'Pearl Crystal', 'HP354', 'headband 16.jpg '),
+    new Items ('headband', 'Silver Rhinestone', 'HP414', 'headband 17.jpg'),
+    new Items ('headband', 'Platinum Plated', 'SL-GSHS0117', 'headband 18.jpg'),
+    new Items ('headband', 'Rhinestone Headband', 'HP501', 'headband 19.jpg'),
+    new Items ('headband', 'Elegant Banquet', 'HP472', 'headband 20.jpg'),
+    new Items ('headband', '24K Gold Plated Zircon', 'Model 164#', 'headband 21.jpg'),
+    new Items ('headband', 'Flower pearl Crystal', 'HP357 - 2', 'headband 22.jpg'),
+    new Items ('headband', 'Platinum Plated Zircon', 'Model 164# - 2', 'headband 23.jpg'),
+    new Items ('headband', 'Ceramic Flower Headband', 'W-TS0110-1', 'headband 24.jpg'),
+    new Items ('headband', 'Rhinestone/Alloy flower', 'HP240', 'headband 25.jpg'),
+    new Items ('headband', 'Pearls Beads Baroque', 'HB6596 - 2', 'headband 26.jpg'),
+    new Items ('headband', 'Rhinestone Headband', 'HP351 - 2', 'headband 27.jpg'),
+    new Items ('headband', 'Rhinestone Headband', 'HP388', 'headband 28.jpg'),
+    new Items ('headband', 'Platinum/ 24K Gold Plated, Zircon - 1', 'Platinum/ 24K Gold Plated, Zircon - 1', 'headband 30.jpg'),
+    new Items ('headband', 'Platinum/ 24K Gold Plated, Zircon - 2', 'Platinum/ 24K Gold Plated, Zircon - 2', 'headband 31.jpg'),
+    new Items ('headband', 'Rhinestone Headband', 'HP404', 'headband 32.jpg'),
+    new Items ('headband', 'Elegant Banquet', 'HP473', 'headband 33.jpg'),
+    new Items ('headband', 'Rhinestone Headband', 'HP479', 'headband 34.jpg'),
+    new Items ('headband', 'Pearl Tassel Headband', 'SA01', 'headband 35.jpg'),
+    new Items ('headband', 'Rhinestone Headband', 'HP450', 'headband 41.jpg'),
+    new Items ('headband', 'Rhinestone/Alloy flower', 'HP284', 'headband 45.jpg'),
+    new Items ('headband', 'Rhinestone Headband', 'HP388 - 5', 'headband 46.jpg'),
+    new Items ('headband', 'Rhinestone Headband', 'HP404 - 66', 'headband 47.jpg'),
+    new Items ('headband', 'Platinum Plated Zircon', 'Model 164# - 3', 'headband 48.jpg'),
+    new Items ('headband', 'Pearl Headband', 'BC6094', 'headband 49.jpg'),
+    new Items ('headband', 'Platinum/ 24K Gold Plated Zircon', 'BC6094 - 2', 'headband 50.jpg'),
+    new Items ('headband', 'Rhinestone Headband', 'HP479 - 9', 'headband 51.jpg')
+];
+const rita_comb = [
+    new Items ('rita_comb', 'Comb - 1', 'Comb - 1', 'rita-comb 39.jpg'),
+    new Items ('rita_comb', 'Comb - 2', 'Comb - 2', 'rita-comb 40.jpg'),
+    new Items ('rita_comb', 'Comb - 3', 'Comb - 3', 'rita-comb 41.jpg'),
+    new Items ('rita_comb', 'Comb - 4', 'Comb - 4', 'rita-comb 42.jpg'),
+    new Items ('rita_comb', 'Comb - 5', 'Comb - 5', 'rita-comb 43.jpg'),
+    new Items ('rita_comb', 'Clip with flower', '11338', 'rita-comb 44.jpg'),
+    new Items ('rita_comb', 'Comb with flower', '94028', 'rita-comb 45.jpg'),
+    new Items ('rita_comb', 'Comb - 6', '96083', 'rita-comb 46.jpg'),
+    new Items ('rita_comb', 'Comb - 7', 'Comb - 7', 'rita-comb 47.jpg'),
+    new Items ('rita_comb', 'Comb - 8', 'Comb - 8', 'rita-comb 48.jpg'),
+    new Items ('rita_comb', 'Comb - 9', '96252', 'rita-comb 49.jpg'),
+    new Items ('rita_comb', 'Comb - 10', '96513', 'rita-comb 50.jpg'),
+    new Items ('rita_comb', 'Comb - 11', '96284', 'rita-comb 51.jpg'),
+    new Items ('rita_comb', 'Comb - 12', '96593', 'rita-comb 52.jpg'),
+    new Items ('rita_comb', 'Comb - 13', '96535', 'rita-comb 53.jpg'),
+    new Items ('rita_comb', 'Comb - 14', '94404', 'rita-comb 54.jpg'),
+    new Items ('rita_comb', 'Comb - 15', '94518', 'rita-comb 55.jpg'),
+    new Items ('rita_comb', 'Comb - 16', '94170', 'rita-comb 56.jpg'),
+    new Items ('rita_comb', 'Comb - 17', '96084', 'rita-comb 57.jpg'),
+    new Items ('rita_comb', 'Comb - 18', '94766', 'rita-comb 58.jpg'),
+    new Items ('rita_comb', 'Comb - 19', '93042', 'rita-comb 59.jpg'),
+    new Items ('rita_comb', 'Comb - 20', '94385', 'rita-comb 60.jpg'),
+    new Items ('rita_comb', 'Comb - 21', '92601', 'rita-comb 61.jpg'),
+    new Items ('rita_comb', 'Comb - 22', '92706', 'rita-comb 62.jpg'),
+    new Items ('rita_comb', 'Comb - 23', '96536', 'rita-comb 63.jpg'),
+    new Items ('rita_comb', 'Comb - 24', '96327', 'rita-comb 64.jpg'),
+    new Items ('rita_comb', 'Comb - 25', '96427', 'rita-comb 65.jpg'),
+    new Items ('rita_comb', 'Comb - 26', '96391', 'rita-comb 66.jpg'),
+    new Items ('rita_comb', 'Comb - 27', '94707', 'rita-comb 67.jpg'),
+    new Items ('rita_comb', 'Comb - 28', '96545', 'rita-comb 68.jpg'),
+    new Items ('rita_comb', 'Comb - 29', '93995', 'rita-comb 69.jpg')
+];
+const rita_crown = [
+    new Items ('rita_crown', 'Crown', '96872', 'rita-crown 39.jpg'),
+    new Items ('rita_crown', 'Crown', '92231', 'rita-crown 40.jpg'),
+    new Items ('rita_crown', 'Tiara', '94527', 'rita-crown 41.jpg'),
+    new Items ('rita_crown', 'Tiara', '92209', 'rita-crown 42.jpg'),
+    new Items ('rita_crown', 'Tiara', '96953', 'rita-crown 43.jpg'),
+    new Items ('rita_crown', 'Tiara', '93072', 'rita-crown 44.jpg'),
+    new Items ('rita_crown', 'Tiara', '94526', 'rita-crown 45.jpg'),
+    new Items ('rita_crown', 'Tiara', '96286', 'rita-crown 46.jpg'),
+    new Items ('rita_crown', 'Tiara', '95145', 'rita-crown 47.jpg'),
+    new Items ('rita_crown', 'Tiara', '95924', 'rita-crown 48.jpg'),
+    new Items ('rita_crown', 'Tiara', '95998', 'rita-crown 49.jpg'),
+    new Items ('rita_crown', 'Tiara', '95923', 'rita-crown 50.jpg'),
+    new Items ('rita_crown', 'Tiara', '96799', 'rita-crown 51.jpg'),
+    new Items ('rita_crown', 'Tiara', '96823', 'rita-crown 52.jpg'),
+    new Items ('rita_crown', 'Tiara', '95922', 'rita-crown 53.jpg'),
+    new Items ('rita_crown', 'Tiara', '96983', 'rita-crown 54.jpg'),
+    new Items ('rita_crown', 'Tiara', '96463', 'rita-crown 56.jpg'),
+    new Items ('rita_crown', 'Tiara', '95925', 'rita-crown 57.jpg'),
+    new Items ('rita_crown', 'Tiara', '97738', 'rita-crown 58.jpg'),
+    new Items ('rita_crown', 'Tiara', '97737', 'rita-crown 59.jpg'),
+    new Items ('rita_crown', 'Tiara', '97745', 'rita-crown 60.jpg'),
+    new Items ('rita_crown', 'Tiara', '97704', 'rita-crown 61.jpg'),
+    new Items ('rita_crown', 'Tiara', '97703', 'rita-crown 62.jpg'),
+    new Items ('rita_crown', 'Tiara', '97613', 'rita-crown 63.jpg'),
+    new Items ('rita_crown', 'Tiara', '97603', 'rita-crown 64.jpg'),
+    new Items ('rita_crown', 'Tiara', '97601', 'rita-crown 65.jpg'),
+    new Items ('rita_crown', 'Tiara', '97595', 'rita-crown 66.jpg'),
+    new Items ('rita_crown', 'Tiara', '97581', 'rita-crown 67.jpg'),
+    new Items ('rita_crown', 'Tiara', '97510', 'rita-crown 68.jpg'),
+    new Items ('rita_crown', 'Tiara', '97282', 'rita-crown 69.jpg'),
+    new Items ('rita_crown', 'Tiara', '97693', 'rita-crown 70.jpg'),
+    new Items ('rita_crown', 'Tiara', '95519', 'rita-crown 71.jpg'),
+    new Items ('rita_crown', 'Tiara', '94656', 'rita-crown 72.jpg'),
+    new Items ('rita_crown', 'Tiara', '94540', 'rita-crown 73.jpg'),
+    new Items ('rita_crown', 'Tiara', '95043', 'rita-crown 74.jpg')
+];
+
+const items = [bracelet, comb, crown, flower, glasses, headband, rita_comb, rita_crown];
+
+function showCateg(){
+    nav = document.getElementById('categories-menu');
+    if(nav.style.display == 'none'){
+        nav.style.display = 'block';
+    }else{
+        nav.style.display = 'none';
+    }
+}
+
+function showAll(){
+    section = document.getElementById('body-cat-section');
+    res = "";
+    for(let i = 0; i < items.length; i++){
+        for(let j = 0; j < items[i].length; j++){
+            res += "<div class='" + items[i][j].type + "'><label for='" + items[i][j].code + "'><img src='./medias/images/" + items[i][j].type + "/" + items[i][j].src + "'></label>";
+            res += "<br/><br/><input type='checkbox' id='" + items[i][j].code + "' name='" + items[i][j].type + "[]' value='" + items[i][j].name + "'>" + items[i][j].name + "</div>";
+        }
+    }
+    section.innerHTML = res;
+}
+function placeAll(){
+    section = document.getElementById('body-cat-section');
+    res = "";
+    for(let i = 0; i < items.length; i++){
+        for(let j = 0; j < items[i].length; j++){
+            res += "<div class='" + items[i][j].type + "' style='display: none;'><label for='" + items[i][j].code + "'><img src='./medias/images/" + items[i][j].type + "/" + items[i][j].src + "'></label>";
+            res += "<br/><br/><input type='checkbox' id='" + items[i][j].code + "' name='" + items[i][j].type + "[]' value='" + items[i][j].name + "'>" + items[i][j].name + "</div>";
+        }
+    }
+    section.innerHTML = res;
+
+}
+function show(cl){
+    if(cl == 'all'){
+        showAll();
+    }else{
+        for(let i = 0; i < items.length; i++){
+            var a = document.querySelectorAll('.'+items[i][0].type);
+            if(items[i][0].type != cl){
+                for(let j = 0; j < a.length; j++){
+                    a[j].style.display = "none";
+                }
+            } else{
+                for(let j = 0; j <a.length; j++){
+                    a[j].style.display = "block";
+                }
+            }
+        }
+    }
+    document.getElementById('orderNow').innerHTML = '<div id="popup"></div> <input id="order-btn" type="button" value="Order Now" name="Order Now" onclick="orderNow()"/>';
+    document.getElementById('aboutUs').style.display = 'none';
+}
---
+++ js/for-order.js
@@ -0,0 +1,32 @@
+function orderNow(){
+    const popup = document.getElementById('popup');
+    popup.style.display = 'block';
+    
+    const checkboxes = document.querySelectorAll('input[type=checkbox]');
+    var res = "<h2>Your Order:</h2><h3>You can check the items you don't want anymore :</h3><table><tr><th>Item name :</th><td>Item code :</td></tr>";
+    for(var checkbox of checkboxes){
+        if(checkbox.checked == true){
+            res += "<tr><th><input id='" + checkbox.id + "' class='popup-checkbox' type='checkbox' name='order[]' value='" + checkbox.value + "'/> " + checkbox.value + "</th><td>" + checkbox.id + "</td></tr>";
+        }
+    } res += "</table><input id='popup-next-btn' type='button' value='Next' name='next step button' onclick='nextStepOrder()'/>";
+    popup.innerHTML = res;
+}
+function nextStepOrder(){
+    const popup = document.getElementById('popup');
+    const checkboxes = document.querySelectorAll('.popup-checkbox');
+    var res = "<h2>This is your FINAL Order:</h2><h3>You can now copy your order to clipboard and go to 'Contact us' section to send us your order</h3><textarea id='orderText'>Hi, I'm looking to buy from your shop these items : ";
+    var sequence = 1;
+    for(var checkbox of checkboxes){
+        if(checkbox.checked == false){
+            res += "Item number " + sequence++ + " : " + checkbox.value + ". Code : " + checkbox.id + ". ";
+        } 
+    } res += "I'm grateful !</textarea><br/><input id='popup-submit-btn' type='button' value='Copy to clipboard' name='ok btn' onclick='copyOrder()'/>";
+    popup.innerHTML = res;
+    
+}
+function copyOrder(){
+    const text = document.querySelector("#orderText");
+    text.select();
+    text.setSelectionRange(0, 99999);
+    document.execCommand('copy');
+}
